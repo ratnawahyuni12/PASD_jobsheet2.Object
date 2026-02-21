690dd94f553723b221ab7737ea124d5084acc941
ratnawahyuni12 <ratna120307@gmail.com>
2026-02-22 02:53:27 +0700

    Modifikasi Percobaan 1 Perintah Nomor 4

--- Mahasiswa20.java
+++ Mahasiswa20.java
@@ -16,7 +16,11 @@ public class Mahasiswa20 {
     }
 
     void updateIPK(double ipkBaru) {
-        ipk = ipkBaru;
+        if (ipkBaru >= 0.0 && ipkBaru <= 4.0) {
+            ipk = ipkBaru;
+        } else {
+            System.out.println("IPK tidak valid. Harus antara 0.0 dan 4.0");
+        }
     }
 
     String nilaiKinerja() {
