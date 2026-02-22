73ff6aa2391a76ea8f0ad47f77fd5077c7293dc0
ratnawahyuni12 <ratna120307@gmail.com>
2026-02-22 08:44:32 +0700

    Percobaan 3: Membuat Konstruktor (langkah 2)

--- MahasiswaMain20.java
+++ MahasiswaMain20.java
@@ -10,5 +10,9 @@ public class MahasiswaMain20 {
         mhs1.ubahKelas("SI 2K");
         mhs1.updateIPK(3.60);
         mhs1.tampilkanInformasi();
+
+        Mahasiswa20 mhs2 = new Mahasiswa20("Annisa Nabila", "2541720160", 3.25, "TI 2L");
+        mhs2.updateIPK(3.30);
+        mhs2.tampilkanInformasi();
     }
 }
