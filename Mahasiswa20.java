26df5bc04dad4d9d93d3a46f5a4505caf298135c
ratnawahyuni12 <ratna120307@gmail.com>
2026-02-22 09:11:35 +0700

    Modifikasi Percobaan 3 Perintah Nomor 3

--- Mahasiswa20.java
+++ Mahasiswa20.java
@@ -4,9 +4,6 @@ public class Mahasiswa20 {
     String kelas;
     double ipk;
 
-    public Mahasiswa20() {
-    }
-
     public Mahasiswa20(String nm, String nim, double ipk, String kls) {
         nama = nm;
         this.nim = nim;
